542d728b928494af9d6c9d8c7444818d8a7e3551
Dustin Burns <dburns@ucdavis.edu>
2014-05-06 14:41:41 -0700

    adding files

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cp ~/../mulhearn/Delphes-3.0.10.tar .
+tar -xf Delphes-3.0.10.tar
+rm Delphes-3.0.10.tar
+cd Delphes-3.0.10/
+make
+cd ..
+cd include/
+ln -s ../Delphes-3.0.10/classes/ classes
+ln -s ../Delphes-3.0.10/external/ExRootAnalysis/ ExRootAnalysis
+cd ../lib/
+ln -s ../Delphes-3.0.10/libDelphes.so libDelphes.so
+cd ..
+cd run
+ln -s ../lib/ .
+source COMPILE
+cd ..
+echo 'hxx setup complete!'
